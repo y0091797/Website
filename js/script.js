60d9bd026c1fe3923838b1dfab708197b9da7421
Moritz Richter <mor1tzr1chter@web.de>
2023-08-30 17:59:45 +0200

    new structure

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(link of tablinks){
+        link.classList.remove("active-link");
+    }
+    for(content of tabcontents){
+        content.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.top = "0";
+}
+
+function closemenu(){
+    sidemenu.style.top = "-100%";
+}
